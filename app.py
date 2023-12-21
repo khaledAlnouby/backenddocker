adf95bbb62d8c148e0c20cb6430179c703c1d333
khaledAlnouby <121772428+khaledAlnouby@users.noreply.github.com>
2023-12-21 10:57:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,36 @@ client = MongoClient(mongo_uri)
 db = client.get_database()
 users_collection = db.get_collection('users')
 
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+    # Check if the email already exists
+    existing_user = users_collection.find_one({'email': data['email']})
+    if existing_user:
+        return jsonify({"msg": "User with this email already exists"}), 400
+
+    new_user = {
+        'email': data['email'],
+        'password': data['password'],
+        'userType': 'doctor' if data.get('isDoctor') else 'patient',
+    }
+
+    users_collection.insert_one(new_user)
+    print("User inserted successfully")
+    return jsonify({"msg": "User registered successfully"}), 201
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if 'email' not in data:
+        return jsonify({"msg": "Email not provided"}), 400
+
+    user = users_collection.find_one({'email': data['email']})
+    if user and user['password'] == data['password']:
+        return jsonify({"msg": "Login successful", "userType": user.get('userType', 'patient')}), 200
+    else:
+        return jsonify({"msg": "Invalid email or password"}), 401
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
